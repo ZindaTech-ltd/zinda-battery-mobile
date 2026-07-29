ee712aca9f73673716d030327c2824e64ae907c8
Ali <aliiiiahmaaeed@gmail.com>
2026-07-29 10:47:15 +0500

    feat: add Supabase client and verify connection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,10 +4,21 @@ import FlowDiagram from '@/components/FlowDiagram';
 import InfoStrip from '@/components/InfoStrip';
 import StatGrid from '@/components/StatGrid';
 import { C } from '@/constants/batteryTheme';
-import React from 'react';
+import { supabase } from '@/utils/supabase';
+import React, { useEffect } from 'react';
 import { ScrollView, StatusBar, StyleSheet, View } from 'react-native';
 
 export default function LiveMonitor() {
+  useEffect(() => {
+    supabase
+      .from('any_table_name') // for now will give an error, but will confirm connection to supabase
+      .select('*')
+      .limit(1)
+      .then(({ data, error }) => {
+        if (error) console.log('SUPABASE ERROR:', error.message);
+        else console.log('SUPABASE CONNECTED:', data);
+      });
+  }, []);
   return (
     <View style={s.root}>
       <StatusBar barStyle="dark-content" backgroundColor={C.bg} />
